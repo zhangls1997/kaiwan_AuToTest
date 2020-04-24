29b8a1019ae78dd9a1e3dd15c9a99668f08f7451
zhangls1997 <zhanglusheng123@gmail.com>
2020-04-24 22:14:20 +0800

    uodate

---
+++ tasks/test_four.py
@@ -0,0 +1,20 @@
+#!/user/bin/python
+#!-*- coding:utf-8 -*-
+from collections import namedtuple
+Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
+Task.__new__.__defaults__ = (None, None, False, None)
+def test_asdict():
+    """ _asdict() should return a dictionary."""
+    t_task = Task('do something', 'okken', True, 21)
+    t_dict = t_task._asdict()
+    expexted = {'summary': 'do something',
+                'owner': 'okken',
+                'done': True,
+                'id': 21}
+    assert t_dict == expexted
+def test_replace():
+    """replace() should change passed in fields"""
+    t_before = Task('finish book', 'brian', False)
+    t_after = t_before._replace(id=10, done=True)
+    t_expected = Task('finish book', 'brian', True, 10)
+    assert t_after == t_expected
--- test_xuexi.py
+++ test_xuexi.py
@@ -1,6 +1,7 @@
 #!/user/bin/python
 #!-*- coding:utf-8 -*-
 import pytest
+from time import sleep
 def add(a,b):
     return a+b
 def zhang(a,b):
@@ -10,34 +11,57 @@ def lu(a,b):
 def sheng(a,b):
     return a/b
 class Testcase(object):
+    @pytest.mark.run_these_please
     def test_one(self):
         print("\n正在执行-----test_one")
         assert add(1,2) == 3    #add 是上面函数的名称
+    @pytest.mark.run_these_please
     def test_two(self):
         print("正在执行-----test_two")
         assert zhang(1,2) == -1
         assert zhang(3,2) == 1    #哈哈
     def test_three(self):
         print("正在执行-----test_three")
+        sleep(1.0)
         assert lu(1,2) == 2
     def test_thour(self):
         print("正在执行-----test_thour")
         assert sheng(6,2) == 3
-if __name__ == "__main__":
-    pytest.main(["-s","test_xuexi.py"])
+# if __name__ == "__main__":
+    # pytest.main(["-s",'-v',"test_xuexi.py"])#运行文件
+    # pytest.main(['--collect-only'])    #  已运行的全部用例情况
+    # pytest.main(['-k','one or two','--collect-only','test_xuexi.py'])  #希望要运行用例的情况
+    # pytest.main(['-k', 'one or two','test_xuexi.py']) #指定运行用例
+    # pytest.main(['-v', '-k','one or two', '--collect-only'])  #详细的要运行用例的情况
+    # pytest.main(['-v','-m','run_these_please',"test_xuexi.py"])  #打标记，只执行带有标记的用例配合pytest.main.mark.标记名使用
+                                                                   #也可以使用多个标记名如：-m "标记名1 and 标记名2"
+                                                                   #或-m "标记名1 and not 标记名2" 过滤掉标记名2的用例
+                                                                   #使用-m "标记名1 or 标记名2"选中所有的用例运行
+    # pytest.main(['-x','test_xuexi.py'])    #遇到失败的测试用例，当前停止
+    # pytest.main(['--tb=no','test_xuexi.py'])   #信息回溯
+    # pytest.main(['--maxfail=1','test_xuexi.py'])   # 遇到失败的测试用例，全局停止，后面的数字代表可以遇到几个失败的测试用例
+    # pytest.main(['--maxfail=2', '--tb=no','test_xuexi.py'])  #错误堆栈回溯
+    # pytest.main(['-l', 'test_xuexi.py'])    #失败后，输出函数局部变量和全局变量的值
+    # pytest.main(['--tb=no', 'test_xuexi.py']) #信息回溯，输出详细的失败原因，三个参数：short、line、no
+    # pytest.main(['--tb=line', 'test_xuexi.py'])  # 告诉错误位置，可以发现他们失败的共性
+    # pytest.main(['--tb=short', 'test_xuexi.py'])    #比前两种更详细，还有三种模式可选：1、--tb=long:信息最详细
+    # pytest.main(['--tb=long', 'test_xuexi.py'])            #2、--tb=auto  默认值，多个用例失败，仅打印最后一个和第一个
+    # pytest.main(['--tb=auto', 'test_xuexi.py'])            #3、--tb=native只输出python标准库的回溯信息
+    # pytest.main(['--tb=nativa', 'test_xuexi.py'])
+    # pytest.main(['--durations=0', 'test_xuexi.py'])  #显示测试用例的call、setup、teardown的耗时
+    
 
-""""
-import requests
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'}
-r = requests.get("https://wwww.baidu.com",headers=headers)
-r.content.decode('utf-8')
-print(r.content.decode("utf-8"))
-"""
+
+# import requests
+# headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'}
+# r = requests.get("https://wwww.baidu.com",headers=headers)
+# r.content.decode('utf-8')    #指定解码格式为utf - 8
+# print(r.content.decode("utf-8"))
 # try:
 #     pass
 # except:
 #     pass
-# else:
+# else:    #try和except都没有才执行else语句
 #     pass
 # finally:
-#     print(1)
+#     print(1)  #无论上面结果如何都执行  finally语句
